fa65a172d8c0964eac2b1bfc93158f3e44c67feb
Ahmed <bousbiaa213@gmail.com>
2025-02-17 19:52:23 +0100

    Home page fixed

---
+++ src/app/components/Home/Home.jsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    return (
+
+      <>
+      
+
+          <img 
+            src="image-home/aa-removebg-preview.png"
+            className="ml-[55%] h-[55%] animate-sabat"
+          />
+    
+
+          {/* Promo Section */}
+        <div className="bg-black text-center py-20 mt-[-34%] h-[80vh] animate-zoomIn  overflow-x-hidden sm:mt-[-100%] md:mt-[-35%]" >
+
+            <h1 className="text-4xl md:text-6xl font-bold text-white tracking-wide uppercase mb-4 animate-font">
+              Step Into Comfort and Style
+            </h1>
+            <p className="text-lg  text-gray-200 mb-6 animate-font sm:text-3xl ">
+              Discover the latest collections designed for every adventure.
+            </p>
+            <button 
+              onClick={() => navigate("/order-sammuray")}
+              className="mt-4 bg-white text-yellow-400 font-bold py-3 px-6 rounded-lg shadow-md hover:bg-gray-100 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
+            >
+              Shop Now
+            </button>
+          </div>
+
+           <div>
+            
+          </div>
+        
+
+           </>
+        
+         
+
+      );
+    
+};
+
+
+export default HomePage;
+
---
+++ src/app/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/Home/Home";
+import Product from "./components/product/Product";
+import Bag from "./components/product/Bag";
+import Hoodie from "./components/product/Hoodie";
+import Jacket from "./components/product/Jacket";
+import  Shoes  from "./components/product/Shoes";
+
+const App = () => {
+    return (
+        <>
+            <Router>
+
+                <Routes>
+                    <Route 
+                        path="/" 
+                        element={
+                            <>
+                             <HomePage />
+                             <Product />
+                            </>
+                        } 
+                    />
+                    <Route path="/product/bag" element={<Bag/>} />
+                    <Route path="/product/hoodie" element={<Hoodie/>} />
+                    <Route path="/product/jacket" element={<Jacket/>} />
+                    <Route path="/product/shoes" element={<Shoes/>} />
+                 
+                </Routes>
+                
+            </Router>
+
+           
+
+   
+     
+            
+        </>
+    );
+};
+
+export default App;
